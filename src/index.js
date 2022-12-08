7c178c0c982aef4e98f9df9dca72d3619bbb9f7e
haomingjue <haomingjue@outlook.com>
2022-12-08 04:53:18 -0500

    updated api

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client"
 
 const client = new ApolloClient({
-    uri: "https://ec2-18-212-2-78.compute-1.amazonaws.com/graphql",
+    uri: "http://ec2-18-212-2-78.compute-1.amazonaws.com/graphql",
     cache: new InMemoryCache(),
 })
 
